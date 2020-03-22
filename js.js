1e4b5baceaa4fc8710d38b9bf29e02354164c61b
jhash-github <54217818+jhash-github@users.noreply.github.com>
2020-03-22 01:11:16 +0100

    -

--- js.js
+++ js.js
@@ -44,7 +44,7 @@ function loadData() {
                 let estimated_cases = attribute.Estimated;
                 let lastUpdated = new Date(attribute.Last_Update);
 
-                let radius = Math.log2(estimated_cases) * 10000;
+                let radius = estimated_cases / 10 + 100000;
 
                 addCase(lat, lon, radius, name, active_cases, deaths, estimated_cases, lastUpdated);
             }
